3adad5bbc1cd469e265e2123ae947e016f4d9db0
chris bolivar <christopherbolivar@hotmail.com>
2019-08-25 17:42:48 -0400

    started to add CSS styles. App up and running with API,Bootstrap,Click events. features and css corrections needed

--- app.js
+++ app.js
@@ -29,9 +29,17 @@ document.querySelector("#getDef").addEventListener('click', () => {
         axios.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + vWord[i] + '?key=ee67d907-7224-4430-a548-5dffdc6214eb').then((dictionaryapi) => {
 
             vDiv.innerHTML += `
-        <h2>${dictionaryapi.data[0].meta.stems[0]}</h2>
-        <p>${dictionaryapi.data[0].fl} </p> 
-        <p>${dictionaryapi.data[0].shortdef[0]} </p> 
+        <div id="card" class="photo card bg-light mb-3">
+			    <div class="side-a flash card-body">
+			      <h3 id="wordFront">${dictionaryapi.data[0].meta.stems[0]}</h3>
+			      </div>
+			      <div  class="side-b flash ">
+                 <h3>${dictionaryapi.data[0].meta.stems[0]}</h3>
+                 <hr class="hr1">
+                 <p>${dictionaryapi.data[0].fl}</p> 
+                 <p>${dictionaryapi.data[0].shortdef[0]} </p> 
+	            </div>
+			    </div>
         `
         })
     }
