65fd16deed303adc1da669d67409a5fc98211bc6
chris bolivar <christopherbolivar@hotmail.com>
2019-08-26 15:20:21 -0400

    flashcard are now cross browser compaitble

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ document.querySelector("#strQuiz").addEventListener('click', () => {
                 `
         <div id="div${i}" class="col-12 mt-2">
         <div class="row">
-        <label class="col">Word:</label> 
+        <label class="col mt-1">Word:</label> 
         <input class="col" id="input${i}" type="text">
         <button type="button" class="button${i} col btn btn-danger ml-1">
         <i class="fa fa-trash"></i>
@@ -61,20 +61,27 @@ document.querySelector("#getDef").addEventListener('click', () => {
     for (i = 0; i < vWord.length; i++) {
 
         axios.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + vWord[i] + '?key=ee67d907-7224-4430-a548-5dffdc6214eb').then((dictionaryapi) => {
+            dArr = [];
             for (let i = 0; i < dictionaryapi.data[0].shortdef.length; i++) {
                 dArr.push(dictionaryapi.data[0].shortdef[i])
             }
-            console.log()
-            vDiv.innerHTML += ` <div class = "photo card bg-light mb-3" >
-                        <div class = "side-a flash card-body" >
-                        <h3 id = "wordFront"> ${dictionaryapi.data[0].meta.stems[0]} </h3> 
-                        </div>
-                         <div class = "side-b flash ">
-                        <h3> ${dictionaryapi.data[0].meta.stems[0]} </h3> 
-                        <hr class = "hr1" >
-                        <p> ${dictionaryapi.data[0].fl}, ${dictionaryapi.data[0].hwi.hw}, ${dictionaryapi.data[0].hwi.prs[0].mw} </p>  
-                        <p> ${dArr.join(" , ") } </p>  
-                        </div> </div>
+            console.log(dArr)
+            vDiv.innerHTML += ` 
+                        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+    ${dictionaryapi.data[0].meta.stems[0]}
+    </div>
+    <div class="flip-card-back">
+      
+    <div class = "side-b flash ">
+    <h3> ${dictionaryapi.data[0].meta.stems[0]} </h3> 
+    <hr class = "hr1" >
+    <p> ${dictionaryapi.data[0].fl}, ${dictionaryapi.data[0].hwi.hw}, ${dictionaryapi.data[0].hwi.prs[0].mw} </p>  
+    <p> ${dArr.join() } </p>  
+    </div>
+  </div>
+</div>
                         `
         })
     }
